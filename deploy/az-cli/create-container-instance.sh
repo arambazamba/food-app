40b191f55fa905691df11e716b777f0b1017e927
Alexander Pajer <alexander.pajer@integrations.at>
2022-10-10 16:36:53 +0200

    sh

---
+++ deploy/az-cli/create-container-instance.sh
@@ -0,0 +1,11 @@
+env=$1
+grp=az400-food-$env
+loc=westeurope
+app=foodui
+img=$2
+dns=$3
+usr=$4
+pwd=$5
+
+az group create -n $grp -l $loc
+az container create -g $grp -l $loc -n $app --image $img --cpu 1 --memory 1 --dns-name-label $dns --port 80 --registry-username $usr --registry-password $pwd
