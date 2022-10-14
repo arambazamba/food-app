12ed85a498285078231ab32b5ad62b1f1b65eb29
Alexander Pajer <alexander.pajer@integrations.at>
2022-10-14 20:41:48 +0200

    aci create

--- deploy/az-cli/create-container-instance.sh
+++ deploy/az-cli/create-container-instance.sh
@@ -1,10 +1,10 @@
     grp=az400-m03-container
     loc=westeurope
     app=food-catalog-api-aci
-    img=$2
-    dns=$3
-    usr=$4
-    pwd=$5
+    img=$1
+    dns=$2
+    usr=$3
+    pwd=$4
 
     az group create -n $grp -l $loc
     az container create -g $grp -l $loc -n $app --image $img --cpu 1 --memory 1 --dns-name-label $dns --port 80 --registry-username $usr --registry-password $pwd
