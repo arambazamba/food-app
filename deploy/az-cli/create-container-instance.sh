d868bc25fc8b32fc573fa506a0cb837e165a36c1
Alexander Pajer <alexander.pajer@integrations.at>
2022-10-14 19:57:30 +0200

    sc renamed

--- deploy/az-cli/create-container-instance.sh
+++ deploy/az-cli/create-container-instance.sh
@@ -1,11 +1,10 @@
-env=$1
-grp=az400-food-$env
-loc=westeurope
-app=foodcatalogapi
-img=$2
-dns=$3
-usr=$4
-pwd=$5
+    grp=az400-m03-container
+    loc=westeurope
+    app=food-catalog-api-aci
+    img=$2
+    dns=$3
+    usr=$4
+    pwd=$5
 
-az group create -n $grp -l $loc
-az container create -g $grp -l $loc -n $app --image $img --cpu 1 --memory 1 --dns-name-label $dns --port 80 --registry-username $usr --registry-password $pwd
+    az group create -n $grp -l $loc
+    az container create -g $grp -l $loc -n $app --image $img --cpu 1 --memory 1 --dns-name-label $dns --port 80 --registry-username $usr --registry-password $pwd
