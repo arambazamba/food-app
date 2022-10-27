455563e9d35c57a4ed897ee114530bae08779772
Alexander Pajer <alexander.pajer@integrations.at>
2022-10-27 09:13:23 +0200

    restructure-completed

--- apps/shop-ui/src/app/menus/menus.module.ts
+++ apps/shop-ui/src/app/menus/menus.module.ts
@@ -2,15 +2,14 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../material.module';
-import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
-const comps = [LoginComponent, NavbarComponent, SidebarComponent];
+const comps = [NavbarComponent, SidebarComponent];
 
 @NgModule({
   declarations: comps,
   exports: comps,
   imports: [CommonModule, MaterialModule, RouterModule],
 })
-export class CoreModule {}
+export class MenusModule {}
