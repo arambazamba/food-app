d72abf971df3fb291a4036f0b7ab9ff9bdf6d1e8
Alexander Pajer <alexander.pajer@integrations.at>
2022-11-04 10:03:32 +0100

    signout effect

--- apps/shop-ui/src/app/auth/state/auth.effects.ts
+++ apps/shop-ui/src/app/auth/state/auth.effects.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { MsalService } from '@azure/msal-angular';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { mergeMap } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { catchError, map, mergeMap } from 'rxjs/operators';
+import { AuthActions } from '../../../../dist/food-shop-ui/src/app/auth/state/auth.actions';
 
 @Injectable()
 export class AuthEffects {
-  constructor(private actions$: Actions, private msals: MsalService) {}
+  constructor(private actions$: Actions, private msal: MsalService) {}
 
   //   tryLoginSilent$ = createEffect(() =>
   //     this.actions$.pipe(
@@ -15,4 +17,16 @@ export class AuthEffects {
   //       })
   //     )
   //   );
+
+  logout$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AuthActions.logout),
+      mergeMap(() =>
+        this.msal.logout().pipe(
+          map((resp) => AuthActions.logoutsuccess()),
+          catchError((err) => of(AuthActions.autherror({ err })))
+        )
+      )
+    )
+  );
 }
